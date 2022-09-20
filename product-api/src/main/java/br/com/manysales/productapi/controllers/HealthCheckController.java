b03d7f8ed346a38cf3625f55c9a719a4606fe39a
Cleyton Sousa <cleytonsouza808@gmail.com>
2022-09-19 22:55:35 -0300

    inital project base for product-api

---
+++ product-api/src/main/java/br/com/manysales/productapi/controllers/HealthCheckController.java
@@ -0,0 +1,25 @@
+package br.com.manysales.productapi.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/check")
+public class HealthCheckController {
+
+    @GetMapping()
+    public ResponseEntity<HashMap<String, Object>> getStatus(){
+        HashMap<String, Object> response = new HashMap<String, Object>();
+
+        response.put("service", "product-api");
+        response.put("application-status", "up");
+        response.put("service-online", HttpStatus.OK);
+        return ResponseEntity.ok(response);
+    }
+}
